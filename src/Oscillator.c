f1aa4a2bcb43f9def0b35be9350a6186160d38db
maximiliankc <54353771+maximiliankc@users.noreply.github.com>
2023-04-16 09:48:00 +1000

    2-synth2 - added osc magnitude normalisation

--- src/Oscillator.c
+++ src/Oscillator.c
@@ -21,16 +21,25 @@ void osc_step(Oscillator * self, float * yr, float * yj) {
     // y[n] = e^(j*theta)*y[n-1]
     // alternatively, like a matrix:
     // (yr[n]  = (cos(theta) -sin(theta) (yr[n-1]
-    //  yi[n]) =  sin(theta)  cos(theta)) yi[n-1])
+    //  yj[n]) =  sin(theta)  cos(theta)) yj[n-1])
     uint8_t i;
+    float pwr;
+    float scale;
 
     // use the state to calculate the first samples in the block
     yr[0] = self->c*self->yrPrev - self->s*self->yjPrev;
     yj[0] = self->s*self->yrPrev + self->c*self->yjPrev;
 
+    // need to normalise the power, otherwise magnitude will drift due to numerical error
+    // once per block should be enough (depending on block length!)
+    pwr = yr[0]*yr[0] + yj[0]*yj[0];
+    // scale is 1st order taylor series approximation of inverse square root
+    scale = 1.5 - 0.5*pwr;
+    yr[0] = scale*yr[0];
+    yj[0] = scale*yj[0];
+
     // calculate the rest of the block
-    for(i = 1; i < BLOCK_SIZE; i++)
-    {
+    for (i = 1; i < BLOCK_SIZE; i++) {
         yr[i] = self->c*yr[i-1] - self->s*yj[i-1];
         yj[i] = self->s*yr[i-1] + self->c*yj[i-1];
     }
