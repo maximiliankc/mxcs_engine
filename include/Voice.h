d72eead23ccc02d4d13befe52b1f7836c00aea36
maximiliankc <cornmaxi@gmail.com>
2023-06-18 12:52:37 +1000

    add voice test function, correct typo

--- include/Voice.h
+++ include/Voice.h
@@ -11,6 +11,6 @@ typedef struct Voice_t {
 void voice_init(Voice_t * self, float a, float d, float s, float r, float f);
 void voice_step(Voice_t * self, float * out);
 void voice_press(Voice_t * self, float f);
-void voice_relese(Voice_t * self);
+void voice_release(Voice_t * self);
 
 #endif // define VOICE_H_
--- test/test.c
+++ test/test.c
@@ -4,6 +4,7 @@
 #include "Oscillator.h"
 #include "Envelope.h"
 #include "Utils.h"
+#include "Voice.h"
 
 
 void test_oscillator(const float f, const unsigned int n, float * cosOut, float * sinOut) {
@@ -51,6 +52,41 @@ void test_envelope(const float a, const float d, const float s, const float r,\
         env_step(&env, envOut + i);
     }
 }
+
+
+void test_voice(const float a, const float d, const float s, const float r, const float f,\
+                   const unsigned int presses, unsigned int pressNs[],\
+                   const unsigned int releases, unsigned int releaseNs[],\
+                   const unsigned int n, float envOut[]) {
+    // parameters:  a: attack time (in samples)
+    //              d: decay time (in samples)
+    //              s: sustain level (amplitude between 0 and 1)
+    //              r: release time (in samples)
+    //              f: frequency to run at (normalised)
+    //              pressNs: times at which to press
+    //              presses: number of presses
+    //              releaseNs: times at which to release
+    //              releaseNs: number of releases
+    //              n: number of samples to iterate over.
+    //                  if n is not a multiple of block_size, the last fraction of a block won't be filled in
+    //              envOut: generated envelope
+    Voice_t voice;
+    unsigned int pressCount = 0;
+    unsigned int releaseCount = 0;
+    voice_init(&voice, a, d, s, r, f);
+    for(unsigned int i=0; i+BLOCK_SIZE <= n; i+= BLOCK_SIZE) {
+        if(pressCount < presses && i >= pressNs[pressCount]) {
+            voice_press(&voice, f);
+            pressCount++;
+        }
+        if(releaseCount < releases && i >= releaseNs[releaseCount]) {
+            voice_release(&voice);
+            releaseCount++;
+        }
+        voice_step(&voice, envOut + i);
+    }
+}
+
 
 void test_db2mag(const unsigned int n, float inOut[]) {
     // parameters: inOut: input/output array
