b23d1e74c7e81787b0ac6f20880e3e776b886f8f
maximiliankc <cornmaxi@gmail.com>
2023-06-25 16:37:23 +1000

    Define a mono synth

--- include/Constants.h
+++ include/Constants.h
@@ -2,5 +2,6 @@
 #define CONSTANTS_H
 
 #define BLOCK_SIZE 16
+#define NOTES 128
 
 #endif // CONSTANTS_H
---
+++ include/Synth.h
@@ -0,0 +1,19 @@
+#ifndef SYNTH_H_
+#define SYNTH_H_
+
+#include <stdint.h>
+#include "Voice.h"
+#include "Constants.h"
+
+// Defining a monophonic synth for now
+typedef struct Synth_t {
+    Voice_t voice;
+    float frequency_table[NOTES];
+} Synth_t;
+
+void synth_init(Synth_t * self);
+void synth_press(Synth_t * self, uint8_t note);
+void synth_release(Synth_t * self);
+void synth_step(Synth_t * self, float * out);
+
+#endif // SYNTH_H_
