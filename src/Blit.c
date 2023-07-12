5866b3724d112d5754a1771bf5fddb11b8fa9d7d
maximiliankc <cornmaxi@gmail.com>
2023-07-13 00:04:05 +1000

    initial parts of tests implemented

--- src/Blit.c
+++ src/Blit.c
@@ -1,20 +1,26 @@
 /* MXCS synthesizer core BLIT implementation
    copyright Maximilian Cornwell 2023
 */
-#include "Blit.h"
+#include <stdio.h>
 
+#include "Blit.h"
 #include "Constants.h"
 
-#define THRESHOLD (0.0000000000001f) // TODO figure out the best threshold
+#define THRESHOLD (1e-6f) // TODO figure out the best threshold
 
-void msinc(float * out, float * lfSin, float * lfCos, float * hfSin, float * hfCos, float M) {
+void msinc(float * out, float * lfCos, float * lfSin, float * hfCos, float * hfSin, float M) {
     for(uint8_t i = 0; i<BLOCK_SIZE; i++) {
-        if (lfSin[i] > THRESHOLD || lfSin[i] < -THRESHOLD) {
-            out[i] = hfSin[i]/(M*lfSin[i]);
+        printf("%d %g %g %g %g", i, lfSin[i], hfSin[i], lfCos[i], hfCos[i]);
+        if (lfCos[i] > THRESHOLD || lfCos[i] < -THRESHOLD) {
+            out[i] = hfCos[i]/(M*lfCos[i]);
+            printf(" . ");
         } else {
-            out[i] = hfCos[i]/lfCos[i];
+            out[i] = hfSin[i]/lfSin[i];
+            printf(" - ");
         }
+        printf("%g\n", out[i]);
     }
+    printf("\n----------------\n");
 }
 
 float blit_m(float f) {
@@ -22,3 +28,30 @@ float blit_m(float f) {
     period = 2*period + 1;
     return (float)period;
 }
+
+#ifdef SYNTH_TEST_
+#include "Oscillator.h"
+
+void test_blit(float * out, float f, unsigned int samples) {
+    float lfSin[BLOCK_SIZE];
+    float lfCos[BLOCK_SIZE];
+    float hfSin[BLOCK_SIZE];
+    float hfCos[BLOCK_SIZE];
+    Oscillator_t lfOsc;
+    Oscillator_t hfOsc;
+
+    osc_init(&lfOsc);
+    osc_init(&hfOsc);
+    float M = blit_m(f);
+    osc_setF(&lfOsc, f/2);
+    osc_setF(&hfOsc, M*f/2);
+    printf("M = %g\n", M);
+    for(unsigned int i = 0; i < samples-BLOCK_SIZE; i+=BLOCK_SIZE) {
+        printf("%d\n", i);
+        osc_step(&lfOsc, lfCos, lfSin);
+        osc_step(&hfOsc, hfCos, hfSin);
+        msinc(&out[i], lfCos, lfSin, hfCos, hfSin, M);
+    }
+}
+
+#endif
--- test/test_blit.py
+++ test/test_blit.py
@@ -1,38 +1,88 @@
 ''' Tests for Band limited impulse train functions
     copyright Maximilian Cornwell 2023 '''
 import ctypes
+import unittest
+import matplotlib.pyplot as plt
 import numpy as np
 
-from .constants import sampling_frequency
+from .constants import sampling_frequency, block_size
 
 class BlitInterface:
     ''' Interface for BLIT functions '''
+    testlib = ctypes.CDLL('test.so')
+
     def __init__(self):
         ''' Load in the test object file and define the function '''
-        self.testlib = ctypes.CDLL('test.so')
         self.float_pointer = ctypes.POINTER(ctypes.c_float)
         self.testlib.blit_m.argtypes = [ctypes.c_float]
         self.testlib.blit_m.restype = ctypes.c_float
-        self.testlib.msinc.argtypes = [self.float_pointer, self.float_pointer,
-                                        self.float_pointer, self.float_pointer,
-                                        self.float_pointer, ctypes.c_int16]
+        self.testlib.test_blit.argtypes = [self.float_pointer, ctypes.c_float, ctypes.c_int]
 
-    def run_msinc(self, freq: float, mod_ratio: int, num_samples: int) -> np.ndarray:
+    def run_blit(self, freq: float, num_samples: int) -> np.ndarray:
         ''' wrapper around msinc function, generates its own co/sines '''
-        n = np.arange(num_samples)
-        lf_sin = np.sin(np.pi*freq*n/sampling_frequency)
-        lf_cos = np.cos(np.pi*freq*n/sampling_frequency)
-        hf_sin = np.sin(np.pi*mod_ratio*freq*n/sampling_frequency)
-        hf_cos = np.cos(np.pi*mod_ratio*freq*n/sampling_frequency)
-        out = np.zeros(n)
-        p_lf_sin = lf_sin.ctypes.data_as(self.float_pointer)
-        p_lf_cos = lf_cos.ctypes.data_as(self.float_pointer)
-        p_hf_sin = hf_sin.ctypes.data_as(self.float_pointer)
-        p_hf_cos = hf_cos.ctypes.data_as(self.float_pointer)
+        out = np.zeros(num_samples, dtype=np.single)
         p_out = out.ctypes.data_as(self.float_pointer)
-        self.testlib.msinc(p_out, p_lf_sin, p_lf_cos, p_hf_sin, p_hf_cos, mod_ratio)
-        return p_out
+        self.testlib.test_blit(p_out, freq/sampling_frequency, len(out))
+        return out
 
     def run_blit_m(self, freqs: list) -> list:
         ''' Wrapper around msinc function '''
-        return [self.testlib.msinc(f) for f in freqs]
+        return [self.testlib.blit_m(f/sampling_frequency) for f in freqs]
+
+
+class TestBlit(unittest.TestCase, BlitInterface):
+    debug = False
+
+    def __init__(self):
+        BlitInterface.__init__(self)
+        unittest.TestCase.__init__(self)
+
+    def test_blit_m(self):
+        freqs = np.arange(10, 16000, 10)
+        ref_ratio = 2*np.trunc(0.5*sampling_frequency/freqs) + 1
+        calc_ratio = self.run_blit_m(freqs)
+
+        error = ref_ratio-calc_ratio
+
+        if self.debug:
+            _, ax1 = plt.subplots()
+            ax1.plot(calc_ratio, label='Calculated')
+            ax1.plot(ref_ratio, ls=':', label='Reference')
+            ax1.grid()
+            ax1.set_ylabel('Output')
+            ax1.set_title('blit_m')
+            ax1.legend()
+
+            _, ax2 = plt.subplots()
+            ax2.plot(error)
+            ax2.grid()
+            ax2.set_ylabel('Output')
+            ax2.set_title('Error')
+
+            plt.show()
+
+        self.assertEqual(np.median(error), 0)
+        self.assertLessEqual(np.max(abs(error)), 2)
+
+    def test_blit(self):
+        num_samples = 20*block_size
+        freq = 200
+        vector = self.run_blit(freq, num_samples)
+        time = np.arange(num_samples)/sampling_frequency
+
+        if self.debug:
+            _, ax1 = plt.subplots()
+            ax1.plot(vector)
+            ax1.grid(True)
+            plt.show()
+
+
+def main():
+    ''' For Debugging/Testing '''
+    blit_test = TestBlit()
+    blit_test.debug = True
+    blit_test.test_blit_m()
+    blit_test.test_blit()
+
+if __name__=='__main__':
+    main()
