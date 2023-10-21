09bdf656da26e98229a6656ce47e921e7f3c90ff
maximiliankc <54353771+maximiliankc@users.noreply.github.com>
2023-10-21 14:47:45 +1100

    remove print statements from blit code

--- src/Blit.cpp
+++ src/Blit.cpp
@@ -1,7 +1,6 @@
 /* MXCS synthesizer core BLIT implementation
    copyright Maximilian Cornwell 2023
 */
-#include <stdio.h>
 
 #include "Blit.h"
 #include "Constants.h"
@@ -10,17 +9,12 @@ const float threshold = 1e-6f; // TODO figure out the best threshold
 
 void msinc(float * out, float * lfCos, float * lfSin, float * hfCos, float * hfSin, float M) {
     for(uint8_t i = 0; i<blockSize; i++) {
-        printf("%d %g %g %g %g", i, lfSin[i], hfSin[i], lfCos[i], hfCos[i]);
         if (lfCos[i] > threshold || lfCos[i] < -threshold) {
             out[i] = hfCos[i]/(M*lfCos[i]);
-            printf(" . ");
         } else {
             out[i] = hfSin[i]/lfSin[i];
-            printf(" - ");
         }
-        printf("%g\n", out[i]);
     }
-    printf("\n----------------\n");
 }
 
 float blit_m(float f) {
@@ -44,9 +38,7 @@ extern "C" {
 
         lfOsc.set_freq(f/2);
         hfOsc.set_freq(M*f/2);
-        printf("M = %g\n", M);
         for(unsigned int i = 0; i < samples-blockSize; i+=blockSize) {
-            printf("%d\n", i);
             lfOsc.step(lfCos, lfSin);
             hfOsc.step(hfCos, hfSin);
             msinc(&out[i], lfCos, lfSin, hfCos, hfSin, M);
