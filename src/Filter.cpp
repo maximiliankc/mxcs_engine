4a72a8cf1023d7fd5622e272d4ca32663ae8c881
maximiliankc <54353771+maximiliankc@users.noreply.github.com>
2024-08-17 16:53:56 +0200

    implement basic filter test, fix filter implementation problems

--- src/Filter.cpp
+++ src/Filter.cpp
@@ -1,7 +1,6 @@
 /* MXCS Engine Filter implementation
    copyright Maximilian Cornwell 2024
 */
-
 #include "Constants.h"
 #include "Filter.h"
 
@@ -22,10 +21,10 @@ Filter_DFI_t::Filter_DFI_t():
 }
 
 Filter_DFI_t::Filter_DFI_t(float * memory_, float * b_, float * a_, uint32_t order_):
-    x_delay_line(memory_, order_/2), y_delay_line(memory_ + order_/2, order_/2) {
+    x_delay_line(memory_, order_), y_delay_line(memory_ + order_, order_) {
     // memory length needs to be 2*order
-    set_coeffs(b_, a_);
     order = order_;
+    set_coeffs(b_, a_);
 }
 
 void Filter_DFI_t::step(float * in, float * out) {
@@ -33,9 +32,21 @@ void Filter_DFI_t::step(float * in, float * out) {
         out[i] = b[0]*in[i];
         for (uint32_t j=0; j<order; j++) {
             out[i] += b[j+1]*x_delay_line.access(j);
-            out[i] += a[j+1]*y_delay_line.access(j);
+            out[i] -= a[j+1]*y_delay_line.access(j);
         }
         x_delay_line.insert(in[i]);
         y_delay_line.insert(out[i]);
     }
 }
+
+#ifdef SYNTH_TEST_
+extern "C" {
+    void test_filter(unsigned int order, float * memory, float * b, float * a, unsigned int ioLength, float * input, float * output) {
+        Filter_DFI_t filter(memory, b, a, order);
+
+        for(unsigned int i=0; i+blockSize <= ioLength; i+= blockSize) {
+            filter.step(&input[i], &output[i]);
+        }
+    }
+}
+#endif // SYNTH_TEST_
---
+++ test/test_filter.py
@@ -0,0 +1,81 @@
+''' Tests for filters
+    copyright Maximilian Cornwell 2024 '''
+import ctypes
+import unittest
+
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.signal as sig
+
+class FilterInterface:
+    ''' Interface class for filter interfaces '''
+    testlib = ctypes.CDLL('test.so')
+
+    def setUp(self):
+        ''' Load in the test object file and define the function '''
+        float_pointer = ctypes.POINTER(ctypes.c_float)
+        # order, memory, a, b, ioLength, input, output
+        self.testlib.test_filter.argtypes = [ctypes.c_uint, float_pointer,
+                                             float_pointer, float_pointer,
+                                             ctypes.c_uint, float_pointer, float_pointer]
+
+    def run_filter(self, b: np.ndarray, a: np.ndarray, samples_in: np.ndarray) -> np.ndarray:
+        ''' Run the filter '''
+        p_float = ctypes.POINTER(ctypes.c_float)
+        order = max(len(b), len(a)) - 1
+        b = np.pad(b, (0, order + 1 - len(b)))
+        a = np.pad(a, (0, order + 1 - len(a)))
+        b_p = np.array(b, dtype=np.single).ctypes.data_as(p_float)
+        a_p = np.array(a, dtype=np.single).ctypes.data_as(p_float)
+        io_length = len(samples_in)
+        samples_in = np.array(samples_in, dtype=np.single)
+        samples_in_p = samples_in.ctypes.data_as(p_float)
+        memory = np.empty(2*order, dtype=np.single)
+        memory_p = memory.ctypes.data_as(p_float)
+        samples_out = np.empty(io_length, dtype=np.single)
+        samples_out_p = samples_out.ctypes.data_as(p_float)
+        self.testlib.test_filter(order, memory_p, b_p, a_p, io_length, samples_in_p, samples_out_p)
+        return samples_out
+
+
+class TestFilter(FilterInterface, unittest.TestCase):
+    debug = False
+
+    def test_response(self):
+        n = 128*16
+        input_sig = np.random.default_rng(1234).normal(0, 0.5, n)
+
+        for a, b in ([([1, 0, 0], [1, 0, 0]),
+                      ([1, 0, 0], [0, 1, 0]),
+                      ([1, 0, 0], [0, 0, 1]),
+                      ([1, 0.5, 0], [1, 0, 0]),
+                      ([1, 0, 0.5], [1, 0, 0]),
+                      ]):
+            ref = sig.lfilter(b, a, input_sig)
+            out = self.run_filter(b, a, input_sig)
+            if self.debug:
+                error = ref - out
+                _, ax = plt.subplots()
+                ax.plot(input_sig, ls=':', label='input')
+                ax.plot(ref, ls=':', label='reference')
+                ax.plot(out, ls=':', label='output')
+                ax.plot(error, ls=':', label='error')
+                ax.plot()
+                ax.grid(True)
+                ax.legend()
+                ax.set_ylabel('Magnitude')
+                ax.set_xlabel('Time (samples)')
+                ax.set_title(f'{b=}, {a=}')
+                plt.show()
+            np.testing.assert_allclose(ref, out, atol=2**-23)
+
+
+def main():
+    ''' For Debugging/Testing '''
+    filter_test = TestFilter()
+    filter_test.setUp()
+    filter_test.debug = True
+    filter_test.test_response()
+
+if __name__=='__main__':
+    main()
