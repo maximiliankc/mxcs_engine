modify q control to use resonance peak, fix slope/frequency response calculation

test/test_filter.py
@@ -53,7 +53,7 @@ class FilterInterface:
         self.testlib.test_filter(filter_type, order, memory_p, b_p, a_p, io_length, samples_in_p, samples_out_p)
         return samples_out
 
-    def run_lp(self, freq: float, q: float, samples_in: np.ndarray) -> np.ndarray:
+    def run_lp(self, freq: float, res: float, samples_in: np.ndarray) -> np.ndarray:
         ''' Run the biquad in lowpass configuration '''
         io_length = len(samples_in)
         p_float = ctypes.POINTER(ctypes.c_float)
@@ -61,10 +61,10 @@ class FilterInterface:
         samples_in_p = samples_in.ctypes.data_as(p_float)
         samples_out = np.empty(io_length, dtype=np.single)
         samples_out_p = samples_out.ctypes.data_as(p_float)
-        self.testlib.test_lowpass(freq, q, io_length, samples_in_p, samples_out_p)
+        self.testlib.test_lowpass(freq, res, io_length, samples_in_p, samples_out_p)
         return samples_out
 
-    def run_hp(self, freq: float, q: float, samples_in: np.ndarray) -> np.ndarray:
+    def run_hp(self, freq: float, res: float, samples_in: np.ndarray) -> np.ndarray:
         ''' Run the biquad in highpass configuration '''
         io_length = len(samples_in)
         p_float = ctypes.POINTER(ctypes.c_float)
@@ -72,7 +72,7 @@ class FilterInterface:
         samples_in_p = samples_in.ctypes.data_as(p_float)
         samples_out = np.empty(io_length, dtype=np.single)
         samples_out_p = samples_out.ctypes.data_as(p_float)
-        self.testlib.test_highpass(freq, q, io_length, samples_in_p, samples_out_p)
+        self.testlib.test_highpass(freq, res, io_length, samples_in_p, samples_out_p)
         return samples_out
 
 def evaluate_f(x: np.ndarray, y: np.ndarray, f: float) -> complex:
@@ -129,52 +129,51 @@ class TestFilter(FilterInterface, unittest.TestCase):
         input_sig[0] = 1
         freqs, p_impulse = sig.periodogram(input_sig, sampling_frequency, detrend=None)
         for f in [100, 500, 1000, 5000, 10000, 20000]:
-            for q in [0.5, 2**0.5, 1, 5, 10, 20]:
+            for res in [-3, 0, 6, 12, 18, 24]:
                 for ftype, filt in [('lp', self.run_lp), ('hp', self.run_hp)]:
-                    output_sig = filt(f, q, input_sig)
+                    output_sig = filt(f, res, input_sig)
                     _, p_xx = sig.periodogram(output_sig, sampling_frequency, detrend=None)
-                    h = 20*np.log10(p_xx/p_impulse)
+                    h = 10*np.log10(p_xx/p_impulse)
                     if self.debug:
                         fig_t, [ax_t, ax_f] = plt.subplots(2, 1)
                         ax_t.plot(np.arange(N)/sampling_frequency, output_sig, label=ftype)
                         ax_f.semilogx(freqs, h, label=ftype)
-                    with self.subTest(f'{ftype}, {f=}, {q=:2f}'):
+                    with self.subTest(f'{ftype}, {f=}, {res=:2f}'):
                         if self.debug:
                             ax_t.grid(True)
                             ax_f.grid(True)
                             ax_t.set_xlabel('Time (s)')
                             ax_f.set_xlabel('Frequency (Hz)')
-                            ax_t.set_title(f'{f=}, {q=}')
-                            ax_f.axhline(2*20*np.log10(q), ls=':', c='k')
+                            ax_t.set_title(f'{f=}, {res=}')
+                            ax_f.axhline(res, ls=':', c='k')
                             ax_f.axvline(f, ls=':', c='k')
                             if ftype == 'lp':
                                 f_passpand = [0, f/10]
                                 ax_f.fill_between(f_passpand, -3, 3, color='g', alpha=0.3)
                                 if f < 10*sampling_frequency:
-                                    x = -80*np.log10(freqs[freqs>10*f]/f) # slope should be -80 db/decade (?)
+                                    x = -40*np.log10(freqs[freqs>10*f]/f) # slope should be -40 db/decade (?)
                                     ax_f.fill_between(freqs[freqs>10*f], x+3, -100, color='g', alpha=0.3)
                             if ftype == 'hp':
-                                x = 80*np.log10(freqs[freqs<f/10]/f) # slope should be 80 db/decade (?)
+                                x = 40*np.log10(freqs[freqs<f/10]/f) # slope should be 40 db/decade (?)
                                 ax_f.fill_between(freqs[freqs<f/10], x+3, -100, color='g', alpha=0.3)
                                 if 10*f < sampling_frequency:
                                     f_passpand = [10*f, sampling_frequency]
                                     ax_f.fill_between(f_passpand, -3, 3, color='g', alpha=0.3)
                             ax_f.set_xlim(left=20)
                             ax_f.set_ylim(bottom=-100)
-                            ax_t.set_title(f'{ftype}, {f=}, {q=:.2f}')
+                            ax_t.set_title(f'{ftype}, {f=}, {res=:.2f}dB')
                             plt.show()
                             plt.close(fig_t)
-                        expected_f0 = 20*np.log10(q)
                         f0_magnitude = 20*np.log10(np.abs(evaluate_f(input_sig, output_sig, f)))
-                        self.assertAlmostEqual(f0_magnitude, expected_f0, delta=3)
+                        self.assertAlmostEqual(f0_magnitude, res, delta=3)
                         if ftype == 'lp':
                             np.testing.assert_allclose(h[freqs<f/10], 0, atol=3)
                             if f/10 > sampling_frequency:
-                                x = -80*np.log10(freqs[freqs>10*f]/f) # slope should be -80 db/decade (?)
-                                np.testing.assert_array_less(h[freqs>10*f], x+3, err_msg=f'lp {q=}, {f=}')
+                                x = -40*np.log10(freqs[freqs>10*f]/f) # slope should be -40 db/decade
+                                np.testing.assert_array_less(h[freqs>10*f], x+3, err_msg=f'lp {res=}, {f=}')
                         if ftype == 'hp':
-                            x = 80*np.log10(freqs[freqs<f/10]/f) # slope should be 80 db/decade (?)
-                            np.testing.assert_array_less(h[freqs<f/10][2:], x[2:]+3, err_msg=f'hp {q=}, {f=}') # ignore dc
+                            x = 40*np.log10(freqs[freqs<f/10]/f) # slope should be 40 db/decade (?)
+                            np.testing.assert_array_less(h[freqs<f/10][2:], x[2:]+3, err_msg=f'hp {res=}, {f=}') # ignore dc
                             if 10*f < sampling_frequency:
                                 np.testing.assert_allclose(h[freqs>10*f], 0, atol=3)
 
@@ -183,7 +182,7 @@ def main():
     ''' For Debugging/Testing '''
     filter_test = TestFilter()
     filter_test.setUp()
-    # filter_test.debug = True
+    filter_test.debug = True
     filter_test.test_response()
     filter_test.test_biquads()
 